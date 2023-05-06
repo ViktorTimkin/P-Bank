08f7b737aae62d29816ec10817dd48d4e70c6573
ViktorTimkin <113607914+ViktorTimkin@users.noreply.github.com>
2023-05-06 19:55:53 +0400

    Comment

---
+++ src/test/java/com/example/pbank/PBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
